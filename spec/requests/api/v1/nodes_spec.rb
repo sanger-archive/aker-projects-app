f467544ca35d45ea81a08469d4e00ea51df1b605
David Robinson <dr6@sanger.ac.uk>
2017-04-25 14:32:41 +0100

    Test that collections are created at the appropriate points

--- spec/requests/api/v1/nodes_spec.rb
+++ spec/requests/api/v1/nodes_spec.rb
@@ -2,6 +2,13 @@ require 'rails_helper'
 
 RSpec.describe 'API::V1::Nodes', type: :request do
 
+  let(:headers) do
+    {
+      "Content-Type": "application/vnd.api+json",
+      "Accept": "application/vnd.api+json"
+    }
+  end
+
   describe 'GET' do
     before(:each) do
       user = create(:user)
@@ -9,10 +16,7 @@ RSpec.describe 'API::V1::Nodes', type: :request do
 
       node = create(:node, cost_code: "S1234", description: "Here is my node")
 
-      get api_v1_node_path(node), headers: {
-        "Content-Type": "application/vnd.api+json",
-        "Accept": "application/vnd.api+json"
-      }
+      get api_v1_node_path(node), headers: headers
     end
 
     it 'returns a response of ok' do
@@ -41,10 +45,7 @@ RSpec.describe 'API::V1::Nodes', type: :request do
 
       before(:each) do
 
-        get api_v1_nodes_path, params: { "filter[cost_code]": "_none" }, headers: {
-          "Content-Type": "application/vnd.api+json",
-          "Accept": "application/vnd.api+json"
-        }
+        get api_v1_nodes_path, params: { "filter[cost_code]": "_none" }, headers: headers
 
         @json = JSON.parse(response.body, symbolize_names: true)
       end
@@ -58,10 +59,7 @@ RSpec.describe 'API::V1::Nodes', type: :request do
     context 'when using a value of !_none for cost_code' do
 
       before(:each) do
-        get api_v1_nodes_path, params: { "filter[cost_code]": "!_none" }, headers: {
-          "Content-Type": "application/vnd.api+json",
-          "Accept": "application/vnd.api+json"
-        }
+        get api_v1_nodes_path, params: { "filter[cost_code]": "!_none" }, headers: headers
 
         @json = JSON.parse(response.body, symbolize_names: true)
       end
@@ -69,9 +67,98 @@ RSpec.describe 'API::V1::Nodes', type: :request do
       it 'returns on the nodes with a cost code' do
         expect(@json[:data].length).to eql(3)
       end
+    end
+  end
 
+  describe 'creating' do
+    before(:each) do
+      user = create(:user)
+      sign_in user
+
+      @root = create(:node, parent_id: nil, name: 'root')
     end
 
+    context 'when creating a node at level 2' do
+      it 'creates a collection for the node' do
+        params = { data: {
+            type: 'nodes',
+            attributes: { name: 'Bananas' },
+            relationships: { parent: { data: { type: 'nodes', id: @root.id } } },
+          }
+        }
+        expect_any_instance_of(Node).to receive(:set_collection)
+        post api_v1_nodes_path, params: params.to_json, headers: headers
+        expect(response).to have_http_status(:created)
+      end
+    end
+    context 'when creating a node at level 3' do
 
+      before do
+        @prog = create(:node, parent_id: @root.id, name: 'prog')
+      end
+
+      it 'does not create a collection for the node' do
+        params = { data: {
+            type: 'nodes',
+            attributes: { name: 'Bananas' },
+            relationships: { parent: { data: { type: 'nodes', id: @prog.id } } },
+          }
+        }
+        expect_any_instance_of(Node).not_to receive(:set_collection)
+        post api_v1_nodes_path, params: params.to_json, headers: headers
+        expect(response).to have_http_status(:created)
+      end
+    end
+  end
+
+  describe 'updating relationship' do
+    before(:each) do
+      user = create(:user)
+      sign_in user
+      @root = create(:node, parent_id: nil, name: 'root')
+    end
+
+    context 'when moving a node to level 2' do
+      before do
+        @prog = create(:node, parent_id: @root.id, name: 'prog')
+        @node = create(:node, parent_id: @prog.id, name: 'node')
+      end
+
+      it 'creates a collection for the node' do
+        params = { 
+          data: {
+            type: 'nodes',
+            id: @root.id,
+          },
+          relationship: 'parent',
+          node_id: @node.id,
+        }
+        expect_any_instance_of(Node).to receive(:set_collection)
+        patch api_v1_node_relationships_parent_path(@node), params: params.to_json, headers: headers
+        expect(response).to have_http_status(:no_content)
+      end
+    end
+
+    context 'when moving a node to level 3' do
+      before do
+        @prog1 = create(:node, parent_id: @root.id, name: 'prog1')
+        @prog2 = create(:node, parent_id: @root.id, name: 'prog2')
+        @node = create(:node, parent_id: @prog1.id, name: 'node')
+      end
+
+      it 'does not create a collection for the node' do
+        params = { 
+          data: {
+            type: 'nodes',
+            id: @prog2.id,
+          },
+          relationship: 'parent',
+          node_id: @node.id,
+        }
+        expect_any_instance_of(Node).not_to receive(:set_collection)
+        patch api_v1_node_relationships_parent_path(@node), params: params.to_json, headers: headers
+        expect(response).to have_http_status(:no_content)
+      end
+    end
   end
 end
