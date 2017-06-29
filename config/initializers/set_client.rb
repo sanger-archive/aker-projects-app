b14813eb8ffbe3cfdea1511774ccbd3fc7073ef8
Chris Smith <chrissmith987@gmail.com>
2017-06-29 16:12:23 +0100

    Fixed adding ZipkinTracer middleware

--- config/initializers/set_client.rb
+++ config/initializers/set_client.rb
@@ -1,13 +1,14 @@
 Rails.application.config.after_initialize do
   SetClient::Base.site = Rails.application.config.set_url
-   SetClient::Base.connection do |connection|
+  SetClient::Base.connection do |connection|
+    ENV['HTTP_PROXY'] = nil
+    ENV['http_proxy'] = nil
+    ENV['https_proxy'] = nil
+    connection.faraday.proxy ''
     connection.use JWTSerializer
-   end
 
-  if Rails.env.production? || Rails.env.staging?
-    SetClient::Base.connection do |connection|
-      
-      connection.use ZipkinTracer::FaradayHandler
+    if Rails.env.production? || Rails.env.staging?
+      connection.use ZipkinTracer::FaradayHandler, "Set Service"
     end
   end
 end
