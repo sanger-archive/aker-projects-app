cb6cdd6b5a9f1c66473cd67a5a9ba7b299c46465
Harriet Craven <hc6@sanger.ac.uk>
2017-10-18 14:27:56 +0100

    Update seeds/staging.rb to be uptodate

--- db/seeds/staging.rb
+++ db/seeds/staging.rb
@@ -1,8 +1,19 @@
-root = Node.create(name: "root")
+root = Node.new(name: "root", owner_email: 'aker')
+root.save(validate: false)
 
-cancer = Node.create(name: "Cancer, Aging & Somatic Mutations", parent: root)
-cellular_genetics = Node.create(name: "Cellular Genetics", parent: root)
-human_genetics = Node.create(name: "Human Genetics", parent: root)
-infection_genomics = Node.create(name: "Infection Genomics", parent: root)
-malaria = Node.create(name: "Malaria", parent: root)
-pathogens = Node.create(name: "Pathogens", parent: root)
+cancer = Node.new(name: "Cancer, Aging & Somatic Mutations", parent: root, owner_email: 'aker')
+cancer.save(validate: false)
+cellular_genetics = Node.new(name: "Cellular Genetics", parent: root, owner_email: 'aker')
+cellular_genetics.save(validate: false)
+human_genetics = Node.new(name: "Human Genetics", parent: root, owner_email: 'aker')
+human_genetics.save(validate: false)
+infection_genomics = Node.new(name: "Infection Genomics", parent: root, owner_email: 'aker')
+infection_genomics.save(validate: false)
+malaria = Node.new(name: "Malaria", parent: root, owner_email: 'aker')
+malaria.save(validate: false)
+pathogens = Node.new(name: "Pathogens", parent: root, owner_email: 'aker')
+pathogens.save(validate: false)
+
+[cancer, cellular_genetics, human_genetics, infection_genomics, malaria, pathogens].each do |program|
+  program.permissions.create([{permitted: 'world', permission_type: :write}])
+end
