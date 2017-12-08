13c9fa3ba7f146307dc06b007d436347e2c53ade
Alex Cummins <alex.cummins@sanger.ac.uk>
2017-12-08 11:23:32 +0000

    Rubocop

--- app/controllers/nodes_controller.rb
+++ app/controllers/nodes_controller.rb
@@ -26,9 +26,9 @@ class NodesController < ApplicationController
     @node_form = NodeForm.new(node_form_params.merge(owner_email: current_user.email))
 
     if @node_form.save
-      flash[:success] = "Node created"
+      flash[:success] = 'Node created'
     else
-      flash[:danger] = "Failed to create node"
+      flash[:danger] = 'Failed to create node'
     end
     redirect_to node_path(@node_form.parent_id)
   end
@@ -51,7 +51,7 @@ class NodesController < ApplicationController
       @node_form = NodeForm.new(node_form_params)
 
       if @node_form.save
-        format.html { redirect_to node_path(@node.parent_id), flash: { success: "Node updated" }}
+        format.html { redirect_to node_path(@node.parent_id), flash: { success: 'Node updated' } }
         format.json { render json: @node, status: :ok }
       else
         format.html {
@@ -69,12 +69,11 @@ class NodesController < ApplicationController
     @parent_id = @node.parent_id
 
     if @node.deactivate(current_user.email)
-      flash[:success] = "Node deleted"
-      redirect_to node_path(@parent_id)
+      flash[:success] = 'Node deleted'
     else
-      flash[:danger] = @node.errors.empty? ? "This node cannot be deleted." : @node.errors.full_messages.join(" ")
-      redirect_to node_path(@parent_id)
+      flash[:danger] = @node.errors.empty? ? 'This node cannot be deleted.' : @node.errors.full_messages.join(' ')
     end
+    redirect_to node_path(@parent_id)
   end
 
   helper_method :check_write_permission_for_node, :jwt_provided?
@@ -104,5 +103,4 @@ class NodesController < ApplicationController
   def check_write_permission_for_node(node)
     current_user && Ability.new(current_user).can?(:write, node)
   end
-
 end
