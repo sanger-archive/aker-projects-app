41ef4f6aa5e2de83228618bdbcb6578c3a5c50ee
Chris Smith <cs24@sanger.ac.uk>
2018-06-21 16:06:08 +0100

    Add health check

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'nodes#show', id: nil
 
+  health_check_routes
+
   resources :nodes do
     collection do
       get 'list'
