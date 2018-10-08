84b1b0abeb6a343a603d853198b3267af1190138
Eduardo Martin Rojo <emr@sanger.ac.uk>
2018-10-09 00:06:04 +0100

    Configs ubw connection

--- config/initializers/billing_facade_client.rb
+++ config/initializers/billing_facade_client.rb
@@ -10,4 +10,12 @@ Rails.application.config.after_initialize do
     ENV['https_proxy'] = nil
     connection.faraday.proxy ''
   end
+
+  BillingFacadeClient.ubw_connection do |connection|
+    ENV['HTTP_PROXY'] = nil
+    ENV['http_proxy'] = nil
+    ENV['https_proxy'] = nil
+    connection.faraday.proxy ''
+  end
+
 end
